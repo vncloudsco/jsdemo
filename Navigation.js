6a65a33415f59ab48dca5924ef621d5d5ed0ffce
VNC Company <41409442+vncloudsco@users.noreply.github.com>
2020-03-13 13:26:44 +0700

    Create Navigation.js

---
+++ Navigation.js
@@ -0,0 +1,9 @@
+  /*<![CDATA[*/
+    var numPages=5;
+    var firstText ='First';
+    var lastText ='Last';
+    var prevText ='Previous';
+    var nextText ='Next';
+    var urlactivepage=location.href;
+    var home_page="/";
+  /*]]>*/
