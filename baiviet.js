c01252e2112384a063636357af44bb49e538ed20
VNC Company <41409442+vncloudsco@users.noreply.github.com>
2020-03-13 10:43:30 +0700

    Create baiviet.js

---
+++ baiviet.js
@@ -0,0 +1,8 @@
+//<![CDATA[
+var no_image = "https://3.bp.blogspot.com/-Yw8BIuvwoSQ/VsjkCIMoltI/AAAAAAAAC4c/s55PW6xEKn0/s1600-r/nth.png";
+var month_format = [, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+var more_text = "View More";
+var comments_text = "<span>Post </span>Comment";
+var pagenav_prev = "Previous";
+var pagenav_next = "Next";
+//]]>
